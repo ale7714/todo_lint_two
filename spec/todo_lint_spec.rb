6231be50cdb1cecd3e8906bc40ababaab9458ebb
Max Jacobson <max@hardscrabble.net>
2017-01-11 12:58:56 -0500

    output some git info
    trying to figure out what changes solano makes

--- spec/todo_lint_spec.rb
+++ spec/todo_lint_spec.rb
@@ -3,6 +3,8 @@ require "spec_helper"
 
 RSpec.describe TodoLint do
   it "has a version number" do
+    system "git status"
+    system "git diff"
     expect(TodoLint::VERSION).to be_a String
   end
 end
