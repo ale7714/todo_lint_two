903e12476bf106054badadc3d6b4378ad092db38
Max Jacobson <max@hardscrabble.net>
2017-01-11 11:37:38 -0500

    lock to earlier ruby-test-reporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,8 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "todo_lint"
 require "timecop"
 
-require 'simplecov'
-SimpleCov.start
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
 
 # Disable colorful output in specs
 # This makes it easier to test and compare strings
