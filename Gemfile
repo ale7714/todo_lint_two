a1f35027dbdbe4c7486e0a76c2b8ac0b5b6bf4f5
Max Jacobson <max@hardscrabble.net>
2017-01-11 11:31:40 -0500

    Enable simplecov

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem "rspec-legacy_formatters"
 gem "rubocop", "~> 0.38"
 gem "timecop", "~> 0.8"
 gem "yardstick", "~> 0.9"
+gem "simplecov"
 
 # Specify your gem's dependencies in todo_lint.gemspec
 gemspec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,9 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "todo_lint"
 require "timecop"
 
+require 'simplecov'
+SimpleCov.start
+
 # Disable colorful output in specs
 # This makes it easier to test and compare strings
 Rainbow.enabled = false
